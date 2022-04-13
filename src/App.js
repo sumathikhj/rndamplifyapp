b8e92c76cabdccd9b2f172721d3156a41ec5b3c9
sumathikhj <nsumathi2001@gmail.com>
2022-04-14 02:02:47 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Welcome
+         Welcome HCL
         </a>
       </header>
 	   <AmplifySignOut />
