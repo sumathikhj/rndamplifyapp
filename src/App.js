1e525baa32c278537b186664e427366a8b05d8bb
sumathikhj <nsumathi2001@gmail.com>
2022-04-14 01:57:29 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Welcome
         </a>
       </header>
 	   <AmplifySignOut />
